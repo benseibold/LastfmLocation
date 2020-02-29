35ffbb4b76235858fb865cc712cfbddd4f98f381
bseibo61 <brseibol@buffalo.edu>
2020-02-29 17:29:24 -0500

    Added a function to list songs that have been played on repeat.  Sorted songs in places by number of times played

--- MusicMap.py
+++ MusicMap.py
@@ -3,6 +3,34 @@ import glob, os, json
 import datetime
 import bisect
 
+
+
+# Generate list of songs that's played the number of times specified
+def on_repeat(times_played, music_df):
+    last_song = ""
+    count = 0
+    repeat_list = []
+
+    times_played -= 2
+    for song in music_df["song"]:
+        if count >= times_played and last_song == song and song not in repeat_list:
+            print(count, times_played)
+            repeat_list.append(song)
+            count = 0
+
+        if last_song != song:
+            count = 0
+        else:
+            count += 1
+
+        last_song = song
+
+    print(repeat_list)
+
+
+
+
+
 location_path = r'Data\GoogleData1-1-2020\Location History\Semantic Location History\2019'
 all_files = glob.glob(os.path.join('', "*.json"))
 
@@ -10,7 +38,6 @@ full_location_json = []
 for f in glob.glob(os.path.join(r'Data\GoogleData1-1-2020\Location History\Semantic Location History\2019', "*.json")):
     with open(f) as i:
         full_location_json.append(json.load(i))
-        #full_location_json.append(json.load(i))
 
 # Get just the activities for visited places
 places_json = []
@@ -32,25 +59,42 @@ for place in places_json:
 location_name_df = pd.DataFrame(location_name_list)
 location_name_df = location_name_df.sort_values('startTimestamp')
 
-# This claims the location_name_df still isn't sorted
-# Maybe try one month??
-print(location_name_df.index.is_monotonic)
-
 music_df = pd.read_csv(r'Data\lastfm.csv', names=['artist', 'album', 'song', 'date'])
 
 # Convert lastfm time string to timestamp like google has
 music_df['timestamp'] = music_df['date'].apply(lambda date: datetime.datetime.timestamp(datetime.datetime.strptime(date, '%d %b %Y %H:%M')))
+
+# on_repeat(2, music_df)
+
+songsInPlace = {}
 
 for index, song in music_df.iterrows():
 
     bi = bisect.bisect(location_name_df['startTimestamp'].tolist(), song['timestamp'])
     
     if ( (bi > 1) and (song['timestamp'] > location_name_df['startTimestamp'][bi - 1]) and (song['timestamp'] < location_name_df['endTimestamp'][bi - 1]) ):
-        print(song)
-        print(location_name_df['name'][bi - 1])
+        # print(location_name_df['name'][bi - 1])
+        if location_name_df['name'][bi - 1] not in songsInPlace:
+            songsInPlace[location_name_df['name'][bi - 1]] = [song]
+        else:
+            # Append song to existing dict value list
+            songsInPlace[location_name_df['name'][bi - 1]].append(song)
+
+for place_key in songsInPlace:
+    print("##################################")
+    print(place_key)
+    played_in_place_counter = {}
+    for song in songsInPlace[place_key]:
+        if song['song'] in played_in_place_counter.keys():
+            played_in_place_counter[song['song']] += 1
+        else:
+            played_in_place_counter[song['song']] = 1
+    print(played_in_place_counter)
 
 
-print(music_df)
+
+
+
 
 
 
