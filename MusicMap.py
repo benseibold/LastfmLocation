eb82bd16809279c95aec3762275fdf30dedd9431
bseibo61 <brseibol@buffalo.edu>
2020-12-17 14:56:52 -0500

    Messing with plotly maps and added lat/long

--- MusicMap.py
+++ MusicMap.py
@@ -2,6 +2,8 @@ import pandas as pd
 import glob, os, json
 import datetime
 import bisect
+import plotly.express as px
+import itertools
 
 # Generate list of songs that's played the number of times specified
 def on_repeat(times_played, music_df):
@@ -30,9 +32,12 @@ location_path = r'Data\GoogleData1-1-2020\Location History\Semantic Location His
 all_files = glob.glob(os.path.join('', "*.json"))
 
 full_location_json = []
-for f in glob.glob(os.path.join(r'Data\GoogleData1-1-2020\Location History\Semantic Location History\2019', "*.json")):
-    with open(f) as i:
-        full_location_json.append(json.load(i))
+year_list = ['2018', '2019', '2020']
+for year in year_list:
+    # print(r"Data\GoogleData1-1-2020\Location History\Semantic Location History\{0}".format(year))
+    for f in glob.glob(os.path.join(r'Data\GoogleData1-1-2020\Location History\Semantic Location History\{0}'.format(year), "*.json")):
+        with open(f, encoding="utf8") as i:
+            full_location_json.append(json.load(i))
 
 # Get just the activities for visited places
 places_json = []
@@ -53,6 +58,8 @@ for place in places_json:
     temp_dict.update({'startTimestamp': ( float(place['placeVisit']['duration']['startTimestampMs'])/1000 ) })
     temp_dict.update({'endTimestamp': ( float(place['placeVisit']['duration']['endTimestampMs'])/1000 ) })
     temp_dict.update({'name': place['placeVisit']['location']['name']})
+    temp_dict.update({'latitude': place['placeVisit']['location']['latitudeE7']})
+    temp_dict.update({'longitude': place['placeVisit']['location']['longitudeE7']})
     location_name_list.append(temp_dict)
 
 location_name_df = pd.DataFrame(location_name_list)
@@ -75,15 +82,18 @@ for index, song in music_df.iterrows():
     if ( (bi > 1) and (song['timestamp'] > location_name_df['startTimestamp'][bi-1]) and (song['timestamp'] < location_name_df['endTimestamp'][bi-1]) ):
         
         if location_name_df['name'][bi - 1] not in songsInPlace:
-            songsInPlace[location_name_df['name'][bi - 1]] = [song]
-        else:
-            # Append song to existing dict value list
+            songsInPlace[location_name_df['name'][bi - 1]] = [location_name_df['latitude'][bi - 1]]
+            songsInPlace[location_name_df['name'][bi - 1]].append(location_name_df['longitude'][bi - 1])
             songsInPlace[location_name_df['name'][bi - 1]].append(song)
+        else:
+            # append song to existing dict value list
+            songsInPlace[location_name_df['name'][bi - 1]].append(song)
+
 
 # Ranks songs in each place
 for place_key in songsInPlace:
-    print("##################################")
-    print(place_key)
+    # print("##################################")
+    # print(place_key)
     played_in_place_counter = {}
     top_songs_in_place = []
     for song in songsInPlace[place_key]:
@@ -94,14 +104,25 @@ for place_key in songsInPlace:
         ordered_played_in_place_counter = sorted(played_in_place_counter.items(), key=lambda x: x[1], reverse=True)
         top_songs_in_place.append(ordered_played_in_place_counter)
     
-    for i in range(0,5):
-        if i < len(ordered_played_in_place_counter):
-            print(ordered_played_in_place_counter[i])
+    # for i in range(0,5):
+    #     if i < len(ordered_played_in_place_counter):
+    #         print(ordered_played_in_place_counter[i])
+
+us_cities = pd.read_csv("Data\mapTest.csv")
+
+fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
+fig.show()
 
 
-# TODO: as of nov 19 2020, get this working for all years and not just 2019, 
-# Something is wrong with lastfm or google timestamps leading to some songs at wonky places but it can't really be fixed , 
-# bring in new data from lastfm and google, delete files in git that shouldn't be in git
+# TODO:
+# get map working with google data.  lat and long are now in songs in place
+# Something is wrong with lastfm or google timestamps leading to some songs at wonky places but it can't really be fixed,
+# bring in new data from lastfm and google
 
 
 
